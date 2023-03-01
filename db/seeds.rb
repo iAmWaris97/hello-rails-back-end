b32a1316b6ff821c9576a547b2755ee9e0a3fb60
iAmWaris97 <haleem.waris@gmail.com>
2023-03-01 18:24:05 +0430

    Seed messages into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello, from greeting one!",
+  "Hello, from greeting two!",
+  "Hello, from greeting three!",
+  "Hello, from greeting four!",
+  "Hello, from greeting five!"
+]
+
+greetings.each { |message| Greeting.create(message: message) }
